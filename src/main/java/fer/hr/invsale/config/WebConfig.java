b3c78de0491c54645ba92c519796b44d450442b9
Dorotea Dragojević <d.dragojevic2510@gmail.com>
2025-06-15 11:30:15 +0200

    Allowed origins
    Signed-off-by: Dorotea Dragojević <d.dragojevic2510@gmail.com>

--- src/main/java/fer/hr/invsale/config/WebConfig.java
+++ src/main/java/fer/hr/invsale/config/WebConfig.java
@@ -18,7 +18,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins(allowedOrigin)
+                        .allowedOrigins(allowedOrigin.split(","))
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
